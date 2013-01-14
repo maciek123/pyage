3de127a6798d1af4626fd35506b24fed1b7412ea
Maciek Kazirod <maxfcb@gmail.com>
2013-01-14 12:44:02 +0100

    bugfix

--- pyage/main.py
+++ pyage/main.py
@@ -3,6 +3,6 @@ from pyage import inject
 from pyage.Computation import Computation
 
 if __name__ == '__main__':
-    inject.conf = sys.argv[1]
+    inject.config = sys.argv[1]
     computation = Computation()
     computation.run()
