59bda6944cf30402bac363977abcca75bfcac66f
Maciek Kazirod <kazirod.maciej@gmail.com>
2014-11-11 22:28:32 +0100

    cleanup

--- pyage/core/inject.py
+++ pyage/core/inject.py
@@ -1,25 +1,17 @@
 config = None
 
-#TODO refactor
+
 class Inject(object):
     def __init__(self, *args):
-        """
-        If there are decorator arguments, the function
-        to be decorated is not passed to the constructor!
-        """
         self.args = args
 
-    def read_config(self, config):
+    @staticmethod
+    def read_config(config):
         # about importing modules: http://docs.python.org/2/reference/simple_stmts.html#grammar-token-import_stmt
         exec "import " + config + " as conf"
         return conf
 
     def __call__(self, f):
-        """
-        If there are decorator arguments, __call__() is only called
-        once, as part of the decoration process!
-
-        """
 
         def wrapped_f(*args, **kwargs):
             conf = self.read_config(config)
@@ -53,7 +45,7 @@ class InjectOptional(Inject):
                         attr = getattr(conf, conf_arg_name)()
                     setattr(args[0], property_name, attr)
             except:
-                pass #parameter is not mandatory
+                pass  # parameter is not mandatory
             return f(*args, **kwargs)
 
         return wrapped_f
