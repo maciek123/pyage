1494f5a3b1f2966b7b42af1ef8b9049cd2605d0e
Maciek Kazirod <kazirod.maciej@gmail.com>
2014-11-30 00:56:39 +0100

    PyroLocator removed

--- pyage/core/locator.py
+++ pyage/core/locator.py
@@ -1,9 +1,6 @@
 import logging
 from math import sqrt, ceil
 import random
-import Pyro4
-from pyage.core.agent.agent import AGENT
-from pyage.core.inject import Inject
 
 logger = logging.getLogger(__name__)
 
@@ -11,27 +8,6 @@ logger = logging.getLogger(__name__)
 class Locator(object):
     def get_neighbour(self, agent):
         raise NotImplementedError()
-
-
-class Pyro4Locator(Locator):
-    @Inject("ns_hostname")
-    def __init__(self):
-        super(Pyro4Locator, self).__init__()
-
-    def get_neighbour(self, agent):
-        try:
-            random_agent = self.__get_random_agent(agent)
-            logger.debug(random_agent.get_address())
-            return random_agent
-        except:
-            logging.exception('')
-
-    def __get_random_agent(self, a):
-        ns = Pyro4.locateNS(self.ns_hostname)
-        agents = ns.list(AGENT)
-        logger.debug(agents)
-        del agents[AGENT + "." + a.address]
-        return Pyro4.Proxy(random.choice(agents.values()))
 
 
 class RandomLocator(Locator):
