db140f2143ec8a120586779249ac6c43bdbf3abd
Maciek Kazirod <kazirod.maciej@gmail.com>
2014-12-13 23:00:24 +0100

    1.2.0 release

--- pavement.py
+++ pavement.py
@@ -7,7 +7,7 @@ setup(
     name="pyage",
     description="Python Agent-based evolution",
     packages=find_packages(),
-    version="1.1.5",
+    version="1.2.0",
     author="Maciej Kaziród",
     author_email="kazirod.maciej@gmail.com",
     requires=['Pyro4(==4.17)']
